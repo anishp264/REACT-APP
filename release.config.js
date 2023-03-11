d56f9dec139c91709576965a7fb6908f68e4a58c
anishp264 <punnenanish@gmail.com>
2023-03-11 21:32:25 +0200

    fix description

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/anishp264/REACT-APP",
+  plugins: [
+    "@semantic-release/commit-analyzer", //analyzes commit messages
+    "@semantic-release/release-notes-generator", //generates release notes based on commit message description
+    //'@semantic-release/npm', useful if releasing npm packages, not required in this project
+    "@semantic-release/github" //responsible for github release notes
+  ]
+};
